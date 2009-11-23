d5f8b005a1f0873d8b5697583e7ef47f762139d0
Matthias Richter <matthias@dsboard.de>
2009-11-23 23:51:38 +0100

    Changed signal to be a userdata. Signal modulation will be more difficult from now on, but this allows for FFT and thus filters.

--- src/lhc.c
+++ src/lhc.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
 #include <AL/alut.h>
 #include <lua.h>
@@ -9,55 +7,6 @@
 #include "generators.h"
 #include "signal.h"
 
-static const int INTERVAL = (1 << (8 * sizeof(short) - 1)) - 1;
-
-ALuint buffer, source;
-
-int l_play(lua_State* L)
-{
-    double rate, length, value, t;
-    size_t bytes, sample_count, i;
-    short* samples;
-
-    if (!lua_isfunction(L, 1))
-        return luaL_error(L, "Expected argument #1 to be a function!");
-
-    lua_getglobal(L, "defaults");
-    lua_getfield(L, -1, "samplerate");
-    rate = luaL_checknumber(L, -1);
-    lua_pop(L, 1);
-    lua_getfield(L, -1, "length");
-    length = luaL_checknumber(L, -1);
-    lua_pop(L, 2);
-
-    sample_count = rate * length;
-    samples = malloc(sample_count * sizeof(short));
-    if (samples == 0) {
-        lua_close(L);
-        alutExit();
-        fprintf(stderr, "Cannot create sample of %lu bytes", sample_count * sizeof(short));
-        exit(1);
-    }
-
-    for (i = 0; i < sample_count; ++i) {
-        lua_pushvalue(L, 1);
-        lua_pushnumber(L, i / rate);
-        lua_call(L, 1, 1);
-        value = lua_tonumber(L, -1);
-        lua_pop(L, 1);
-        samples[i] = value * INTERVAL;
-    }
-
-    alSourceStop(source);
-    alSourcei(source, AL_BUFFER, 0);
-    alBufferData(buffer, AL_FORMAT_MONO16, samples, sample_count, rate);
-    free(samples);
-
-    alSourcei(source, AL_BUFFER, (ALint)buffer);
-    alSourcePlay(source);
-
-    return 0;
-}
 
 #define SET_DEFAULT(field, value) lua_pushnumber(L, (value)); lua_setfield(L, -2, (field))
 int main(int argc, char** argv)
@@ -66,9 +15,6 @@ int main(int argc, char** argv)
     int error;
     alutInit(&argc, argv);
     alGetError();
-
-    alGenBuffers(1, &buffer);
-    alGenSources(1, &source);
 
     lua_State *L = lua_open();
     luaL_openlibs(L);
@@ -83,8 +29,6 @@ int main(int argc, char** argv)
     SET_DEFAULT("phase", 0);
     lua_setglobal(L, "defaults");
 
-    lua_register(L, "play", l_play);
-
     printf("lhc> ");
     while (fgets(input_buffer, sizeof(input_buffer), stdin) != NULL) {
         error = luaL_loadbuffer(L, input_buffer, strlen(input_buffer), "line") || lua_pcall(L, 0,0,0);
@@ -95,11 +39,6 @@ int main(int argc, char** argv)
         printf("lhc> ");
     }
     lua_close(L);
-
-    alSourceStop(source);
-    alSourcei(source, AL_BUFFER, 0);
-    alDeleteBuffers(1, &buffer);
-    alDeleteSources(1, &source);
 
     alutExit();
     return 0;
--- src/signal.c
+++ src/signal.c
@@ -1,6 +1,20 @@
 #include "signal.h"
 #include <lauxlib.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <math.h>
+#include <AL/alut.h>
+#include <assert.h>
+
+typedef struct Signal__
+{
+    ALuint buffer;
+    ALuint source;
+    int is_generated;
+    /* TODO: player thread */
+} Signal;
+
+static const int INTERVAL = (1 << (8 * sizeof(short) - 1)) - 1;
 
 static int signal_closure(lua_State *L)
 {
@@ -21,7 +35,90 @@ static int signal_closure(lua_State *L)
     return 1;
 }
 
-#define GET_NUMBER_OR_DEFAULT(index, name) do { lua_getfield(L, (index), name); \
+/* TODO: error checking on OpenAL functions */
+int l_play(lua_State* L)
+{
+    double rate, length, value, t;
+    size_t bytes, sample_count, i;
+    short* samples;
+    Signal* signal = lua_touserdata(L, 1);
+    if (signal == NULL)
+        return luaL_error(L, "signal argument expected in signal.play!");
+
+    if (lua_getmetatable(L, 1) == 0)
+        return luaL_error(L, "signal argument expected in signal.play!");
+
+    if (!signal->is_generated) {
+        lua_getglobal(L, "defaults");
+        lua_getfield(L, -1, "samplerate");
+        rate = luaL_checknumber(L, -1);
+        lua_pop(L, 1);
+        lua_getfield(L, -1, "length");
+        length = luaL_checknumber(L, -1);
+        lua_pop(L, 2);
+
+        /* TODO: continus play by double buffering - do so by starting a new thread */
+        sample_count = rate * length;
+        samples = malloc(sample_count * sizeof(short));
+        if (samples == 0) {
+            lua_close(L);
+            alutExit();
+            fprintf(stderr, "Cannot create sample of %lu bytes", sample_count * sizeof(short));
+            exit(1);
+        }
+
+        /* create samples */
+        for (i = 0; i < sample_count; ++i) {
+            lua_getfield(L, 2, "signal");
+            assert(lua_gettop(L) == 3);
+            assert(lua_isfunction(L, -1));
+            lua_pushnumber(L, i / rate);
+            assert(lua_gettop(L) == 4);
+            lua_call(L, 1, 1);
+            assert(lua_gettop(L) == 3);
+            value = lua_tonumber(L, -1);
+            lua_pop(L, 1);
+            assert(lua_gettop(L) == 2);
+            samples[i] = value * INTERVAL;
+        }
+        lua_pop(L, 1);
+
+        /* play samples */
+        alGenBuffers(1, &(signal->buffer));
+        alGenSources(1, &(signal->source));
+        alBufferData(signal->buffer, AL_FORMAT_MONO16, samples, sample_count, rate);
+        signal->is_generated = 1;
+        free(samples);
+        alSourcei(signal->source, AL_BUFFER, (ALint)signal->buffer);
+    }
+    lua_pop(L, 1);
+    alSourcePlay(signal->source);
+
+    return 0;
+}
+
+int l_stop(lua_State *L)
+{
+    Signal* signal = lua_touserdata(L, 1);
+    if (signal == NULL || lua_getmetatable(L, 1) == 0)
+        return luaL_error(L, "signal argument expected in signal.stop!");
+    alSourceStop(signal->source);
+    return 0;
+}
+
+int signal_gc(lua_State *L)
+{
+    Signal* signal = lua_touserdata(L, 1);
+
+    alSourceStop(signal->source);
+    alSourcei(signal->source, AL_BUFFER, 0);
+    
+    alDeleteBuffers(1, &(signal->buffer));
+    alDeleteSources(1, &(signal->source));
+    return 0;
+}
+
+#define GET_NUMBER_OR_DEFAULT(index, sname, name) do { lua_getfield(L, (index), sname); \
     if (!lua_isnumber(L, -1)) { \
         lua_pop(L, 1); \
         lua_getglobal(L, "defaults"); \
@@ -31,29 +128,43 @@ static int signal_closure(lua_State *L)
 
 static int l_signal(lua_State *L)
 {
-    if (!lua_istable(L, -1))
+    if (!lua_istable(L, 1))
         return luaL_error(L, "error: expected table as argument.");
 
-    lua_getfield(L, -1, "g");
-    if (!lua_isfunction(L, -1)) { 
-        lua_pop(L, 1);
-        lua_pushnumber(L, 1);
-        lua_gettable(L, -2);
-        if (!lua_isfunction(L, -1))
-            return luaL_error(L, "error: expected generator to be a function.");
-    }
+    lua_pushnumber(L, 1);
+    lua_gettable(L, 1);
+    if (!lua_isfunction(L, -1))
+        return luaL_error(L, "error: expected generator to be a function.");
 
-    GET_NUMBER_OR_DEFAULT(-2, "freq");
-    GET_NUMBER_OR_DEFAULT(-3, "amp");
-    GET_NUMBER_OR_DEFAULT(-4, "phase");
+    GET_NUMBER_OR_DEFAULT(-2, "f", "freq");
+    GET_NUMBER_OR_DEFAULT(-3, "a", "amp");
+    GET_NUMBER_OR_DEFAULT(-4, "p", "phase");
+    /* stack content: args, generator, freq, amp, phase */
 
+    /* create metatable for signal userdatum */
     lua_pushcclosure(L, &signal_closure, 4); 
-    lua_remove(L, -2);
-    return 1;
+    lua_newtable(L);
+    lua_replace(L, 1); /* stack: table closure */
+    lua_setfield(L, 1, "signal"); /* {signal = closure} */
+    lua_pushcfunction(L, l_play);
+    lua_setfield(L, 1, "play"); /* {signal = closure, play = l_play} */
+    lua_pushcfunction(L, l_stop);
+    lua_setfield(L, 1, "stop"); /* {signal = closure, play = l_play, stop = l_stop} */
+    lua_pushcfunction(L, signal_gc);
+    lua_setfield(L, 1, "__gc"); /* {signal = closure, play = l_play, stop = l_stop, __gc = signal_gc } */
+    lua_pushvalue(L, 1);
+    lua_setfield(L, 1, "__index"); /* this provides OO-access for the userdata */
+
+    Signal* s = lua_newuserdata(L, sizeof(Signal)); /* stack: { signal, play, stop, __gc, __index } Signal */
+    lua_insert(L, 1); /* stack: Signal { signal, play, stop, __gc, __index }*/
+    lua_setmetatable(L, 1);
+    s->is_generated = 0;
+    return 1; /* Signal userdata */
 }
 
 int luaopen_signal(lua_State *L)
 {
     lua_register(L, "signal", l_signal);
+    lua_register(L, "sig", l_signal);
     return 0;
 }
